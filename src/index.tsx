c0df1c7e479bcbbc1fba708455390b7e0dd50ac2
chaithanya <chaithanyachowdary20@gmail.com>
2023-02-23 09:06:23 +0530

    Sunburst in react

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "react-dom";
+
+import { SunburstChart } from "./SunburstChart";
+
+const rootElement = document.getElementById("root");
+render(<SunburstChart />, rootElement);
